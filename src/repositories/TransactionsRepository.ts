678466aaed25e52a4338cda3868ae1c08e642b87
Leonardo Oliveira <leozartino@gmail.com>
2020-04-29 10:56:40 -0300

    feat(TransactionRepository.ts): add getBalance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -18,15 +24,26 @@ class TransactionsRepository {
   }
 
   public getBalance(): Balance {
-    const balance = this.transactions;
-    return balance;
+    const Balance = this.transactions.reduce(
+      (balance, { value, type }) => {
+        if (type === 'income') {
+          balance.income += value;
+        } else {
+          balance.outcome += value;
+        }
+        balance.total = balance.income - balance.outcome;
+        return balance;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    return Balance;
   }
 
-  public create(
-    title: string,
-    value: number,
-    type: 'income' | 'outcome',
-  ): Transaction {
+  public create({ title, value, type }: TransactionDTO): Transaction {
     const transaction = new Transaction({ title, value, type });
     this.transactions.push(transaction);
     return transaction;
