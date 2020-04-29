24afaff0df4f71bba94b572527cc570f42f25f72
Leonardo Oliveira <leozartino@gmail.com>
2020-04-29 10:57:35 -0300

    feat(transaction.routes.ts): add rota GET

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -5,11 +5,13 @@ import { Router } from 'express';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transaction = transactionsRepository.all();
+    const balance = transactionsRepository.balance();
+    return response.json({ transaction, balance });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
